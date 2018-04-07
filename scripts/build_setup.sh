f049888e0c022dff267e4c32a53892b67474c02d
Jesús Arroyo Torrens <jesus.jkhlg@gmail.com>
2018-04-07 11:54:04 +0200

    Fix windows_x86|amd64 builds

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -32,17 +32,17 @@ fi
 if [ $ARCH == "windows_x86" ]; then
   EXE=".exe"
   HOST="i686-w64-mingw32"
-  CONFIG_CFLAGS="-O2"
-  CONFIG_CXXFLAGS="-O2"
-  CONFIG_LDFLAGS="-static -static-libstdc++"
+  MAKE_CFLAGS="-O2"
+  MAKE_CXXFLAGS="-O2"
+  MAKE_LDFLAGS="-static"
 fi
 
 if [ $ARCH == "windows_amd64" ]; then
   EXE=".exe"
   HOST="x86_64-w64-mingw32"
-  CONFIG_CFLAGS="-O2"
-  CONFIG_CXXFLAGS="-O2"
-  CONFIG_LDFLAGS="-static -static-libstdc++"
+  MAKE_CFLAGS="-O2"
+  MAKE_CXXFLAGS="-O2"
+  MAKE_LDFLAGS="-static"
 fi
 
 if [ $ARCH == "darwin" ]; then
